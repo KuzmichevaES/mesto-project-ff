8471f0ec710c158fb60c28c380c7c5a25a8fbe99
Ekaterina Kuzmicheva <kuzmicheva9252@yandex.ru>
2023-10-30 23:00:53 +0300

    to the review

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,31 @@
 
 // @todo: Функция создания карточки
 
+function addCards (card, deleteCard) {
+    
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+};
+
 // @todo: Функция удаления карточки
 
+function deleteCard (event) {
+    const cardListItem = event.target.closest('.places__item');
+    cardListItem.remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+const cardList = document.querySelector('.places__list');
+
+initialCards.forEach(function (card) {
+    let cardElement = addCards(card, deleteCard);
+    cardList.append(cardElement);
+});
